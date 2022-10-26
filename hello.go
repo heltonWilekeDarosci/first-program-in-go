3a088388ef40be5647784dbbb166727017df9edd
helton <heltoloco@hotmail.com>
2022-10-26 10:57:49 -0300

    refactor(hello): simplify the variable names

--- hello.go
+++ hello.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var name string = "Wileke"
-	var age int = 30
-	var version float32 = 1.1
+	name := "Wileke"
+	age := 30
+	version := 1.1
 	fmt.Println("Hello Mr.", name, "your age is", age)
 	fmt.Println("This program is on version", version)
 }
