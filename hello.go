349e561ecc09bb6b2bacf7ccbe6441448cb9328c
helton <heltoloco@hotmail.com>
2022-10-26 10:51:25 -0300

    feat(hello): create basic structure to print lines and use variables

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Wileke"
+	var age int = 30
+	var version float32 = 1.1
+	fmt.Println("Hello Mr.", name, "your age is", age)
+	fmt.Println("This program is on version", version)
+}
