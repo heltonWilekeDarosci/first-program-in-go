5871b546f584ac42b20b17debbc46de09355e0dc
helton <heltoloco@hotmail.com>
2022-10-26 17:20:00 -0300

    feat(hello): apply loop to monitoring func and time of wait between tests

--- hello.go
+++ hello.go
@@ -4,25 +4,28 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
 
 	showIntro()
-	showMenu()
-	command := seeCommand()
+	for {
+		showMenu()
+		command := seeCommand()
 
-	switch command {
-	case 1:
-		startMonitoring()
-	case 2:
-		fmt.Println("Exhibiting Logs...")
-	case 0:
-		fmt.Println("Exiting...")
-		os.Exit(0) // os.Exit(0) To tell to exit because the result was a success, nothing went wrong <<---
-	default:
-		fmt.Println("Command unknown")
-		os.Exit(-1) // os.Exit(-1) To tell something went wrong <<---
+		switch command {
+		case 1:
+			startMonitoring()
+		case 2:
+			fmt.Println("Exhibiting Logs...")
+		case 0:
+			fmt.Println("Exiting...")
+			os.Exit(0) // os.Exit(0) To tell to exit because the result was a success, nothing went wrong <<---
+		default:
+			fmt.Println("Command unknown")
+			os.Exit(-1) // os.Exit(-1) To tell something went wrong <<---
+		}
 	}
 }
 
@@ -48,12 +51,25 @@ func seeCommand() int {
 
 func startMonitoring() {
 	fmt.Println("Monitoring...")
-	site := "http://random-status-code.herokuapp.com/"
-	reply, _ := http.Get(site)
+	websites := []string{"http://random-status-code.herokuapp.com/", "http://www.alura.com.br", "http://www.caellum.com.br"}
+
+	for i := 0; i < 5; i++ {
+		for i, website := range websites {
+			fmt.Println("Testing website", i, ":", website)
+			websiteTest(website)
+		}
+		time.Sleep(10 * time.Second)
+		fmt.Println("")
+	}
+	fmt.Println("")
+}
+
+func websiteTest(website string) {
+	reply, _ := http.Get(website)
 
 	if reply.StatusCode == 200 {
-		fmt.Println("The website:", site, "loaded successfully!")
+		fmt.Println("The website:", website, "loaded successfully!")
 	} else {
-		fmt.Println("The website:", site, "is having problems. Status code:", reply.StatusCode)
+		fmt.Println("The website:", website, "is having problems. Status code:", reply.StatusCode)
 	}
 }
