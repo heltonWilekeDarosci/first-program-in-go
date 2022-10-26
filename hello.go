2f924bbdd0cca2d89a613524d5d31f342df8e444
helton <heltoloco@hotmail.com>
2022-10-26 15:11:19 -0300

    refactor(hello): simplify and organize code

--- hello.go
+++ hello.go
@@ -3,15 +3,35 @@ package main
 import "fmt"
 
 func main() {
+
+	showIntro()
+	showMenu()
+	command := seeCommand()
+
+	switch command {
+	case 1:
+	case 2:
+	case 0:
+	default:
+	}
+}
+
+func showIntro() {
 	name := "Wileke"
 	version := 1.1
 	fmt.Println("Hello Mr.", name)
 	fmt.Println("This program is on version", version)
+}
 
+func showMenu() {
 	fmt.Println("1- Start monitoring")
 	fmt.Println("2- Exhibit Logs")
 	fmt.Println("0- Exit")
+}
 
-	var command int
-	fmt.Scan(&command)
+func seeCommand() int {
+	var commandRead int
+	fmt.Scan(&commandRead)
+
+	return commandRead
 }
