6cfd21e36cae493ed544c2b9835936468f4f0af2
helton <heltoloco@hotmail.com>
2022-10-26 14:23:45 -0300

    feat(hello): creating a menu with Scan function

--- hello.go
+++ hello.go
@@ -4,8 +4,14 @@ import "fmt"
 
 func main() {
 	name := "Wileke"
-	age := 30
 	version := 1.1
-	fmt.Println("Hello Mr.", name, "your age is", age)
+	fmt.Println("Hello Mr.", name)
 	fmt.Println("This program is on version", version)
+
+	fmt.Println("1- Start monitoring")
+	fmt.Println("2- Exhibit Logs")
+	fmt.Println("0- Exit")
+
+	var command int
+	fmt.Scan(&command)
 }
